37d398d47f5468351c9d448c435914c247cc4bb6
Saksham Joshi <150180744+sakshamjosh1@users.noreply.github.com>
2025-08-10 14:29:22 +0530

    Revised basic Java Questions

---
+++ basics/oddeven.java
@@ -0,0 +1,7 @@
+public class oddeven {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        
+    }
+}
